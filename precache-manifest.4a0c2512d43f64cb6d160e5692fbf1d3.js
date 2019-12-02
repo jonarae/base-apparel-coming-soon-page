d4710fc86e80d924d69655de4bdf3752ed501256
Jona <jraeobrador@gmail.com>
2019-12-02 18:58:51 +0800

    Updates

---
+++ precache-manifest.4a0c2512d43f64cb6d160e5692fbf1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e577bb0a73a75ba5392c361c523b4783",
+    "url": "/base-apparel-coming-soon-page/index.html"
+  },
+  {
+    "revision": "a3f4aaf441a857067976",
+    "url": "/base-apparel-coming-soon-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0298687ee747f274af86",
+    "url": "/base-apparel-coming-soon-page/static/js/2.1f4460cd.chunk.js"
+  },
+  {
+    "revision": "a3f4aaf441a857067976",
+    "url": "/base-apparel-coming-soon-page/static/js/main.cca072de.chunk.js"
+  },
+  {
+    "revision": "fac1423514b8baf091e4",
+    "url": "/base-apparel-coming-soon-page/static/js/runtime-main.4e43234d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/base-apparel-coming-soon-page/static/media/logo.25bf045c.svg"
+  }
+]);
